941ff6b34b145e8ad2e0e19192661da155d74d6a
sylvesterlan100 <sylvesterlan100@gmail.com>
2015-02-08 20:43:02 -0800

    angular servdirectives intro

---
+++ angularjs/directives-intro/app.js
@@ -0,0 +1,44 @@
+var app= angular.module('myApp', []);
+
+app.controller('MainController', ['$scope', function($scope){
+	$scope.photo={
+		url: 'duck.jpg',
+		date: 'Jan 1, 2014'
+	};
+}]);
+
+/*
+app.directive('photo', function(){
+	return {
+		//Element directive: a tag
+		restrict: 'E',
+		template: "<figure><img width='500px' /><figcaption /></figure>",
+		//Photo directive is replaced by template
+		replace: true,
+		//Transferring data between html and directive
+		link: function(scope, element, attrs){
+			attrs.$observe('caption', function(value){
+				element.find('figcaption').text(value)
+			})
+			attrs.$observe('photoSrc', function(value){
+				element.find('img').attr('src', value)
+			})
+		}
+	}
+})
+*/
+
+
+//Better way
+app.directive('photo', function(){
+	return {
+		restrict: 'E',
+		template: '<figure>'+'<img width="500px" ng-src="{{photoSrc}}" />'+'<figcaption>{{caption}}</figcaption>'+'</figure>',
+		replace: true,
+		scope:{
+			caption: '@',
+			photoSrc: '@'
+		}
+
+	}
+})
