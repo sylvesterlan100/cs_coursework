19efbb49150577bbe6f9482415465cae47cd86eb
sylvesterlan100 <sylvesterlan100@gmail.com>
2015-02-19 11:02:55 -0800

    directives

---
+++ angularjs/directives-5/app.js
@@ -0,0 +1,29 @@
+var app=angular.module('myApp', []);
+
+app.controller('MainController', function($scope){
+
+})
+
+app.directive('row', function(){
+	return {
+		restrict: 'E',
+		compile: function(tElement, attrs){
+			var content=angular.element('<div class="row"></div>')
+			content.append(tElement.contents());
+			tElement.replaceWith(content);
+		}
+
+	}
+});
+
+app.directive('column', function(){
+	return{
+		restrict: 'E',
+		compile: function(tElement, attrs){
+			var content=angular.element('<div class="col"></div>')
+			content.append(tElement.contents());
+			tElement.replaceWith(content);
+		}
+	}
+
+})
