07df0429532b6ac43a81db93ecdb0475f90882f1
sylvesterlan100 <sylvesterlan100@gmail.com>
2015-02-25 15:16:39 -0800

    more angular

--- angularjs/_practice/promise_practice/app.js
+++ angularjs/_practice/promise_practice/app.js
@@ -7,15 +7,18 @@ app.controller('MainController', function($scope, $q, $interval, $timeout){
 	$scope.mydata2="notify";
 
 	var defer= $q.defer();
-	var defer1=$q.defer();
 
 	defer.promise
 		.then(function(val){$scope.mydata=val}, function(error){$scope.mydata1=error}, function(note){$scope.mydata2=note})
 		
 
+	function timer(){
+		setTimeout(function(){return "done"}, 3000);
+	}
+
 	$timeout(function(){
-		defer.resolve(defer1);
-	}, 3000);
+		defer.resolve("got "+timer());
+	}, 1000);
 
 	
 
